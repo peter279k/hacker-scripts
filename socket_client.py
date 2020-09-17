7aec30228133a75c6446067d72f9c17c178b98f0
Chun-Sheng, Li <peter279k@gmail.com>
2020-09-17 16:52:26 +0800

    Let url and ports are arguments for arg parser (#1)

--- socket_client.py
+++ socket_client.py
@@ -1,13 +1,37 @@
 #!/usr/bin/python3
 
 import socket
+import argparse
 
-ports = [22, 80, 443, 3306]
+parser = argparse.ArgumentParser(description='Please input the port numbers')
+parser.add_argument('--ports', metavar='Y', type=str, help='Please use 3306,80,21 to add ports lists')
+parser.add_argument('--url', metavar='Y', type=str, help='Please set a target URL or IP address')
 
-for port_number in ports:
+args = parser.parse_args()
+
+if args.ports is None:
+    print('Please input following ports: 3306,80,21... and so on.')
+    exit(1)
+
+ports = args.ports
+ports_list = ports.split(',')
+
+ip_address = args.url
+
+if ip_address is None:
+    print('Please set the targeted URL or IP address via --url option')
+    exit(1)
+
+for port_number in ports_list:
+    try:
+        int(port_number)
+    except:
+        print('The port number %s is invalid. Skipped' % port_number)
+        continue
+
     s = socket.socket()
     try:
-        s.connect(('127.0.0.1', port_number))
+        s.connect((ip_address, int(port_number)))
         answer = "Answer for " + str(port_number) + ":"
         print(answer)
         print(s.recv(1024))
